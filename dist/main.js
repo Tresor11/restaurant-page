3b64a5f69ade94bba61b353ab61907d3251d0931
Tresor11 <tresor.moise2001@gmail.com>
2020-03-20 22:45:05 +0200

    added the stickler file

--- dist/main.js
+++ dist/main.js
@@ -8,44 +8,45 @@
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable no-restricted-syntax */
 /* eslint-disable guard-for-in */
+
 !(function (e) {
   const t = {};
-  function n(a) {
-    if (t[a]) return t[a].exports;
-    const c = (t[a] = { i: a, l: !1, exports: {} });
-    return e[a].call(c.exports, c, c.exports, n), (c.l = !0), c.exports;
+  function a(n) {
+    if (t[n]) return t[n].exports;
+    const o = (t[n] = { i: n, l: !1, exports: {} });
+    return e[n].call(o.exports, o, o.exports, a), (o.l = !0), o.exports;
   }
-  (n.m = e),
-  (n.c = t),
-  (n.d = function (e, t, a) {
-    n.o(e, t) || Object.defineProperty(e, t, { enumerable: !0, get: a });
+  (a.m = e),
+  (a.c = t),
+  (a.d = function (e, t, n) {
+    a.o(e, t) || Object.defineProperty(e, t, { enumerable: !0, get: n });
   }),
-  (n.r = function (e) {
+  (a.r = function (e) {
     typeof Symbol !== 'undefined'
         && Symbol.toStringTag
         && Object.defineProperty(e, Symbol.toStringTag, { value: 'Module' }),
     Object.defineProperty(e, '__esModule', { value: !0 });
   }),
-  (n.t = function (e, t) {
-    if ((1 & t && (e = n(e)), 8 & t)) return e;
+  (a.t = function (e, t) {
+    if ((1 & t && (e = a(e)), 8 & t)) return e;
     if (4 & t && typeof e === 'object' && e && e.__esModule) return e;
-    const a = Object.create(null);
+    const n = Object.create(null);
     if (
-      (n.r(a),
-      Object.defineProperty(a, 'default', { enumerable: !0, value: e }),
+      (a.r(n),
+      Object.defineProperty(n, 'default', { enumerable: !0, value: e }),
       2 & t && typeof e !== 'string')
     ) {
-      for (const c in e) {
-        n.d(
-          a,
-          c,
-          ((t) => e[t]).bind(null, c),
+      for (const o in e) {
+        a.d(
+          n,
+          o,
+          ((t) => e[t]).bind(null, o),
         );
       }
     }
-    return a;
+    return n;
   }),
-  (n.n = function (e) {
+  (a.n = function (e) {
     const t = e && e.__esModule
       ? function () {
         return e.default;
@@ -53,20 +54,20 @@
       : function () {
         return e;
       };
-    return n.d(t, 'a', t), t;
+    return a.d(t, 'a', t), t;
   }),
-  (n.o = function (e, t) {
+  (a.o = function (e, t) {
     return Object.prototype.hasOwnProperty.call(e, t);
   }),
-  (n.p = ''),
-  n((n.s = 0));
+  (a.p = ''),
+  a((a.s = 0));
 }([
-  function (_e, t, n) {
-    n.r(t);
-    const a = {
-      create(e, t, n) {
-        const a = document.createElement(e);
-        return (a.className += t), a.setAttribute('id', n), a;
+  function (e, t, a) {
+    a.r(t);
+    const n = {
+      create(e, t, a) {
+        const n = document.createElement(e);
+        return (n.className += t), n.setAttribute('id', a), n;
       },
       getElement(e) {
         return document.getElementById(e);
@@ -87,145 +88,148 @@
         document.getElementById(e).style.display = 'initial';
       },
     };
-    const c = function () {
-      const e = a.create(
+    const o = function () {
+      const e = n.create(
         'div',
         'row py-4 text-center d-flex justify-content-around',
         'nav',
       );
-      const t = a.create('button', 'btn btn-outline-dark', 'homeB');
-      const n = a.create('button', 'btn btn-outline-dark', 'menuB');
-      const c = a.create('button', 'btn btn-outline-dark', 'contactB');
+      const t = n.create('button', 'btn btn-outline-dark', 'homeB');
+      const a = n.create('button', 'btn btn-outline-dark', 'menuB');
+      const o = n.create('button', 'btn btn-outline-dark', 'contactB');
       return (
-        a.updateHtml(t, '<i class="fas fa-glass-martini">HOME</i>'),
-        a.updateHtml(n, '<i class="fas fa-book-open">MENU</i>'),
-        a.updateHtml(c, '<i class="fas fa-phone    ">contact</i>'),
-        [t, n, c].forEach(t => {
-          a.append(e, t);
+        n.updateHtml(t, '<i class="fas fa-glass-martini">HOME</i>'),
+        n.updateHtml(a, '<i class="fas fa-book-open">MENU</i>'),
+        n.updateHtml(o, '<i class="fas fa-phone    ">CONTACT</i>'),
+        [t, a, o].forEach(t => {
+          n.append(e, t);
         }),
         e
       );
     };
-    const o = function () {
-      const e = a.create('div', 'col-12', 'contact');
-      const t = a.create('h4', 'text-center display-4 py-2', 'hHome');
-      a.updateText(t, 'chat with us');
-      const n = a.create('p', 'text-center display-5 py-2', 'hHome');
+    const c = function () {
+      const e = n.create('div', 'col-12', 'contact');
+      const t = n.create('h4', 'text-center display-4 py-2', 'hHome');
+      n.updateText(t, 'chat with us');
+      const a = n.create('p', 'text-center display-5 py-2', 'hHome');
       return (
-        a.updateText(n, 'email:kariburesto@gmail.com'),
-        [t, n].forEach(t => {
-          a.append(e, t);
+        n.updateText(a, 'email:kariburesto@gmail.com'),
+        [t, a].forEach(t => {
+          n.append(e, t);
         }),
         e
       );
-    };
-    const i = function () {
-      const e = a.create('div', 'py-4 row', 'main');
-      const t = a.create('div', 'col-8 row ml-auto', 'cont');
-      return a.append(e, t), e;
     };
     const r = function () {
-      const e = a.create('div', 'col-12', 'home');
-      const t = a.create('h1', 'text-center display-4 py-2', 'hHome');
-      const n = a.create(
+      const e = n.create('div', 'py-4 row', 'main');
+      const t = n.create('div', 'col-8 row ml-auto', 'cont');
+      return n.append(e, t), e;
+    };
+    const i = function () {
+      const e = n.create('div', 'col-12', 'home');
+      const t = n.create('h1', 'text-center display-4 py-2', 'hHome');
+      const a = n.create(
         'p',
         'text-center text-muted font-italic m-auto py-4',
         'intoH',
       );
-      a.updateText(n, "Food isn't mean to just be eaten it should be enjoyed");
-      const c = a.create(
+      n.updateText(a, "Food isn't meant to just be eaten it should be enjoyed");
+      const o = n.create(
         'div',
         'd-flex justify-content-around col-12 py-4',
         'icons',
       );
       [
-        a.create('i', 'fas fa-cookie-bite fa-10x', 'cookie'),
-        a.create('i', 'fas fa-cocktail fa-10x', 'cookie'),
-        a.create('i', 'fas fa-glass-cheers fa-10x', 'cookie'),
+        n.create('i', 'fas fa-cookie-bite fa-10x', 'cookie'),
+        n.create('i', 'fas fa-cocktail fa-10x', 'cookie'),
+        n.create('i', 'fas fa-glass-cheers fa-10x', 'cookie'),
       ].forEach(e => {
-        a.append(c, e);
+        n.append(o, e);
       });
-      const o = a.create(
+      const c = n.create(
         'p',
         'text-center display-4 font-italic m-auto py-4',
         'intoH',
       );
       return (
-        a.updateText(o, 'karibu restaurent is a sprace where you can get a'),
-        a.updateText(t, 'Karibu retaurent'),
-        [t, n, c, o].forEach(t => {
-          a.append(e, t);
+        n.updateText(
+          c,
+          'karibu restaurant is a space where you get quality food',
+        ),
+        n.updateText(t, 'Karibu restaurant'),
+        [t, a, o, c].forEach(t => {
+          n.append(e, t);
         }),
         e
       );
     };
     const s = function () {
-      const e = a.create('div', 'col-12', 'menu');
-      const t = a.create('div', 'row py-2 item', 'item1');
-      const n = a.create('div', 'row py-2 item', 'item1');
-      const c = a.create('div', 'row py-2 item', 'item1');
-      const o = a.create('div', 'row py-2 item', 'item1');
-      const i = a.create('img', 'img-fluid col-6 menu-img', 'menu-img');
-      i.src = '../dist/asset/images/kim-daniels-yItVmeh1XA8-unsplash.jpg';
-      const r = a.create('img', 'img-fluid col-6 menu-img', 'menu-img');
-      r.src = '../dist/asset/images/emerson-vieira-cpkPJ-U9eUM-unsplash (1).jpg';
-      const s = a.create('img', 'img-fluid col-6 menu-img', 'menu-img');
+      const e = n.create('div', 'col-12', 'menu');
+      const t = n.create('div', 'row py-2 item', 'item1');
+      const a = n.create('div', 'row py-2 item', 'item1');
+      const o = n.create('div', 'row py-2 item', 'item1');
+      const c = n.create('div', 'row py-2 item', 'item1');
+      const r = n.create('img', 'img-fluid col-6 menu-img', 'menu-img');
+      r.src = '../dist/asset/images/kim-daniels-yItVmeh1XA8-unsplash.jpg';
+      const i = n.create('img', 'img-fluid col-6 menu-img', 'menu-img');
+      i.src = '../dist/asset/images/emerson-vieira-cpkPJ-U9eUM-unsplash (1).jpg';
+      const s = n.create('img', 'img-fluid col-6 menu-img', 'menu-img');
       s.src = '../dist/asset/images/joseph-gonzalez-fdlZBWIP0aM-unsplash (1).jpg';
-      const d = a.create('img', 'img-fluid col-6 menu-img', 'menu-img');
-      d.src = '../dist/asset/images/victoria-shes-XgFFJKSPkNk-unsplash (1).jpg';
-      const l = a.create('div', 'col-6', 'introM');
-      a.updateHtml(
-        l,
-        '<h4 class="mt-4 d-block text-wrap">legums</h4><p class="mt-4 d-block"><span class="badge badge-primary">price: 10$</span></p>',
+      const l = n.create('img', 'img-fluid col-6 menu-img', 'menu-img');
+      l.src = '../dist/asset/images/victoria-shes-XgFFJKSPkNk-unsplash (1).jpg';
+      const d = n.create('div', 'col-6', 'introM');
+      n.updateHtml(
+        d,
+        '<h4 class="mt-4 d-block text-wrap">legumes</h4><p class="mt-4 d-block">we offer vey high qrality legume extracted directly from our garden</p><p class="mt-4 d-block"><span class="badge badge-primary">price: 10$</span></p>',
       );
-      const p = a.create('div', 'col-6', 'introM');
-      a.updateHtml(
+      const p = n.create('div', 'col-6', 'introM');
+      n.updateHtml(
         p,
-        '<h4 class="mt-4 d-block text-wrap">Grilled meat</h4><p class="mt-4 d-block"><span class="badge badge-primary">price: 50$</span></p>',
+        '<h4 class="mt-4 d-block text-wrap">Grilled meat</h4><p class="mt-4 d-block">get a taste of our fresh grilled meat</p><p class="mt-4 d-block"><span class="badge badge-primary">price: 50$</span></p>',
       );
-      const u = a.create('div', 'col-6', 'introM');
-      a.updateHtml(
+      const u = n.create('div', 'col-6', 'introM');
+      n.updateHtml(
         u,
-        '<h4 class="mt-4 d-block text-wrap">Boiled eggs</h4><p class="mt-4 d-block"><span class="badge badge-primary">price: 20$</span></p>',
+        '<h4 class="mt-4 d-block text-wrap">Boiled eggs</h4><p class="mt-4 d-block">Our eggs are directly etracted from our chicken coop</p><p class="mt-4 d-block"><span class="badge badge-primary">price: 20$</span></p>',
       );
-      const m = a.create('div', 'col-6', 'introM');
+      const m = n.create('div', 'col-6', 'introM');
       return (
-        a.updateHtml(
+        n.updateHtml(
           m,
-          '<h4 class="mt-4 d-block text-wrap">Raw meat</h4><p class="mt-4 d-block"><span class="badge badge-primary">price: 10$</span></p>',
+          '<h4 class="mt-4 d-block text-wrap">Raw meat</h4><p class="mt-4 d-block">if you like meat so much you will enjoy our fresh raw meat</p><p class="mt-4 d-block"><span class="badge badge-primary">price: 10$</span></p>',
         ),
-        [i, l].forEach(e => {
+        [r, d].forEach(e => {
           t.appendChild(e);
         }),
-        [r, p].forEach(e => {
-          n.appendChild(e);
+        [i, p].forEach(e => {
+          a.appendChild(e);
         }),
         [s, u].forEach(e => {
-          c.appendChild(e);
-        }),
-        [d, m].forEach(e => {
           o.appendChild(e);
         }),
-        [t, n, c, o].forEach(t => {
+        [l, m].forEach(e => {
+          c.appendChild(e);
+        }),
+        [t, a, o, c].forEach(t => {
           e.appendChild(t);
         }),
         e
       );
     };
-    const d = a.getElement('content');
-    a.append(d, c()), a.append(d, i());
-    const l = a.getElement('cont');
-    a.append(l, r()),
-    a.append(l, s()),
-    a.append(l, o()),
-    (a.getElement('menuB').onclick = function () {
-      a.hide('home'), a.hide('contact'), a.show('menu');
+    const l = n.getElement('content');
+    n.append(l, o()), n.append(l, r());
+    const d = n.getElement('cont');
+    n.append(d, i()),
+    n.append(d, s()),
+    n.append(d, c()),
+    (n.getElement('menuB').onclick = function () {
+      n.hide('home'), n.hide('contact'), n.show('menu');
     }),
-    (a.getElement('homeB').onclick = function () {
-      a.show('home'), a.hide('menu'), a.hide('contact');
+    (n.getElement('homeB').onclick = function () {
+      n.show('home'), n.hide('menu'), n.hide('contact');
     }),
-    (a.getElement('contactB').onclick = function () {
-      a.hide('home'), a.hide('menu'), a.show('contact');
+    (n.getElement('contactB').onclick = function () {
+      n.hide('home'), n.hide('menu'), n.show('contact');
     });
   },
 ]));
--- src/menu.js
+++ src/menu.js
@@ -15,13 +15,13 @@ function menue() {
   const pic4 = method.create('img', 'img-fluid col-6 menu-img', 'menu-img');
   pic4.src = '../dist/asset/images/victoria-shes-XgFFJKSPkNk-unsplash (1).jpg';
   const description1 = method.create('div', 'col-6', 'introM');
-  method.updateHtml(description1, '<h4 class="mt-4 d-block text-wrap">legums</h4><p class="mt-4 d-block"><span class="badge badge-primary">price: 10$</span></p>');
+  method.updateHtml(description1, '<h4 class="mt-4 d-block text-wrap">legumes</h4><p class="mt-4 d-block">we offer vey high qrality legume extracted directly from our garden</p><p class="mt-4 d-block"><span class="badge badge-primary">price: 10$</span></p>');
   const description2 = method.create('div', 'col-6', 'introM');
-  method.updateHtml(description2, '<h4 class="mt-4 d-block text-wrap">Grilled meat</h4><p class="mt-4 d-block"><span class="badge badge-primary">price: 50$</span></p>');
+  method.updateHtml(description2, '<h4 class="mt-4 d-block text-wrap">Grilled meat</h4><p class="mt-4 d-block">get a taste of our fresh grilled meat</p><p class="mt-4 d-block"><span class="badge badge-primary">price: 50$</span></p>');
   const description3 = method.create('div', 'col-6', 'introM');
-  method.updateHtml(description3, '<h4 class="mt-4 d-block text-wrap">Boiled eggs</h4><p class="mt-4 d-block"><span class="badge badge-primary">price: 20$</span></p>');
+  method.updateHtml(description3, '<h4 class="mt-4 d-block text-wrap">Boiled eggs</h4><p class="mt-4 d-block">Our eggs are directly etracted from our chicken coop</p><p class="mt-4 d-block"><span class="badge badge-primary">price: 20$</span></p>');
   const description4 = method.create('div', 'col-6', 'introM');
-  method.updateHtml(description4, '<h4 class="mt-4 d-block text-wrap">Raw meat</h4><p class="mt-4 d-block"><span class="badge badge-primary">price: 10$</span></p>');
+  method.updateHtml(description4, '<h4 class="mt-4 d-block text-wrap">Raw meat</h4><p class="mt-4 d-block">if you like meat so much you will enjoy our fresh raw meat</p><p class="mt-4 d-block"><span class="badge badge-primary">price: 10$</span></p>');
   [pic1, description1].forEach(el => { item1.appendChild(el); });
   [pic2, description2].forEach(el => { item2.appendChild(el); });
   [pic3, description3].forEach(el => { item3.appendChild(el); });
